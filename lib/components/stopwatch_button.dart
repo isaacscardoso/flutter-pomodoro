feb4dcfc8e71d753e048e43e51262e865d6aa677
isaacscardoso <isaacscardosoblue@outlook.com>
2022-08-03 16:56:52 -0300

    Implementing the 'StopwatchButton' component.

---
+++ lib/components/stopwatch_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:pomodoro_app/components/custom_elevated_button.dart';
+import 'package:pomodoro_app/components/custom_text.dart';
+
+class StopwatchButton extends CustomElevatedButton {
+  final String content;
+
+  const StopwatchButton({
+    Key? key,
+    required this.content,
+    required super.onPressed,
+    required super.icon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomElevatedButton(
+      onPressed: onPressed,
+      primaryColor: Colors.black,
+      shape: const RoundedRectangleBorder(),
+      padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+      child: Row(
+        children: <Widget>[
+          Icon(icon, size: 28),
+          CustomText(content: content, fontSize: 20)
+        ],
+      ),
+    );
+  }
+}
