c574d6437aa09ce7dc6fd83fd7672a9866abbaf4
isaacscardoso <isaacscardosoblue@outlook.com>
2022-08-03 16:58:12 -0300

    Adding the start, stop and restart buttons to the 'Stopwatch' component, and refactoring some styles.

--- lib/components/stopwatch.dart
+++ lib/components/stopwatch.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pomodoro_app/components/custom_text.dart';
+import 'package:pomodoro_app/components/stopwatch_button.dart';
 
 class Stopwatch extends StatelessWidget {
   const Stopwatch({Key? key}) : super(key: key);
@@ -7,19 +8,46 @@ class Stopwatch extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.purpleAccent,
+      color: Colors.red,
       child: Column(
+        mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.center,
-        children: const <Widget>[
-          CustomText(
+        children: <Widget>[
+          const CustomText(
             content: 'Hora de Estudar',
-            fontSize: 40,
+            fontSize: 45,
             textColor: Colors.white,
           ),
-          CustomText(
-            content: '25:00',
-            fontSize: 120,
-            textColor: Colors.white,
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 20),
+            child: CustomText(
+              content: '25:00',
+              fontSize: 120,
+              textColor: Colors.white,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              StopwatchButton(
+                content: 'Iniciar',
+                onPressed: () {},
+                icon: Icons.play_arrow,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: StopwatchButton(
+                  content: 'Parar',
+                  onPressed: () {},
+                  icon: Icons.stop,
+                ),
+              ),
+              StopwatchButton(
+                content: 'Reiniciar',
+                onPressed: () {},
+                icon: Icons.refresh,
+              ),
+            ],
           ),
         ],
       ),
