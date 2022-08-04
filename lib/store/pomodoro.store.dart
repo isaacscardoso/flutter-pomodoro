0f6a675d1bb8351fd4f8386ea9c5b9e2133e0adf
isaacscardoso <isaacscardosoblue@outlook.com>
2022-08-04 14:59:21 -0300

    Setting properties of minutes and seconds, and also the functions of start, pause, reset.

--- lib/store/pomodoro.store.dart
+++ lib/store/pomodoro.store.dart
@@ -1,21 +1,45 @@
 import 'package:mobx/mobx.dart';
+
 part 'pomodoro.store.g.dart';
 
 class PomodoroStore = PomodoroStoreBase with _$PomodoroStore;
 
 abstract class PomodoroStoreBase with Store {
+  @observable
+  bool _started = false;
+
+  @computed
+  bool get started => _started;
+
+  @observable
+  int _minutes = 25;
+
+  @computed
+  int get minutes => _minutes;
+
+  @observable
+  int _seconds = 0;
+
+  @computed
+  int get seconds => _seconds;
 
   @observable
   int _workingTime = 2;
+
+  @computed
+  int get workingTime => _workingTime;
 
   @observable
   int _restTime = 1;
 
   @computed
-  int get workingTime => _workingTime;
-
-  @computed
   int get restTime => _restTime;
+
+  @action
+  void toggleStarted() => _started = !_started;
+
+  @action
+  void reset() => _started = false;
 
   @action
   void incrementWorkingTime() => _workingTime++;
